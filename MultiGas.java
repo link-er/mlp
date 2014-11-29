a9d95c6492a3697e169d020de6f89c61e272f4ca
Moerov <moyorov@gmail.com>
2014-11-29 22:07:55 +0100

    Without learning

--- MultiGas.java
+++ MultiGas.java
@@ -1,7 +1,8 @@
-package neuronet_5;
-
+import java.io.PrintWriter;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -14,78 +15,123 @@ import java.util.Random;
 public class MultiGas {
 	private double[][] patterns;
 	private double[][][] centers;
-	private int M;
-	private int N;
-	private int K;
-	private double nInit = 0.6;
-	private double nFin = 0;
+	private final int M;
+	private final int N;
+	//private int K;
+	private final double nInit = 0.6;
+	private final double nFin = 0;
+	private final double gaussianSize = 0.5;
 	private int tMax;
-	private double gaussianSize = 0.5;
-	
-	public MultiGas(int K, int M, int N, int[] structure) {
-		this.K = K;
+
+	public MultiGas(int K, int M, int N, int tMax, int[] structure) {
+		checkInput(M, structure);
+		//this.K = K;
 		this.M = M;
 		this.N = N;
+		this.tMax = tMax;
 		findPatterns();
-		initializeCenters();
+		initializeCenters(structure);
 	}
 	
 	public MultiGas(int K, int M, int N, String filename, int[] structure) {
-		this.K = K;
+		checkInput(M, structure);
+		//this.K = K;
 		this.M = M;
 		this.N = N;
 		readPatterns(filename);
-		initializeCenters();
+		initializeCenters(structure);
+	}
+
+	private void checkInput(int M, int[] structure){
+		if(M != structure.length){
+			System.out.println("Wrong params: structure.length is not equal M");
+			System.exit(-1);
+		}
 	}
 	
 	private void readPatterns(String inputFilename) {
 		try{
 			BufferedReader br = new BufferedReader(new FileReader(inputFilename));
-			 List<String> lines = Files.readAllLines(Paths.get(inputFilename),
-	                    Charset.defaultCharset());
-				patterns = new double[lines.size()][N];
-				tMax = lines.size();
-				int j = 0;
-	            for (String line : lines) {
-	            	String[] allValues = line.split(" ");
-   					for(int i=0; i<N; i++){
-   						patterns[j][i]= Double.parseDouble(allValues[i]);
-   					}
-   					j++;
-	            }
+			List<String> lines = Files.readAllLines(Paths.get(inputFilename),
+	        Charset.defaultCharset());
+			patterns = new double[lines.size()][N];
+			tMax = lines.size();
+			int j = 0;
+			for (String line : lines) {
+	           	String[] allValues = line.split("  ");
+	           	for(int i=0; i<N; i++){
+   					patterns[j][i]= Double.parseDouble(allValues[i]);
+   				}
+   				j++;
+	        }
 		}
 		catch(IOException e){
-			System.out.println( e.getMessage() );
+			System.out.println(e.getMessage());
 		}
 	}
 	
-	private void initializeCenters() {
-//		TODO rewrite
-//		centers = new double[M][][N];
-//		sizes = new double[K];
-//
-//		//Contains unique indices for centers (chosen from the inputData)
-//		List<Integer> indices = new ArrayList<Integer>();
-//		//Generate K unique indices
-//		Random rand = new Random();
-//		while(indices.size()<K){
-//			int randomIndex = rand.nextInt(inputData.length);
-//			if(indices.contains(randomIndex)){
-//				continue;
-//			}
-//			indices.add(randomIndex);
-//		}
-//		
-//		//Use these indices to initialize centers
-//		for(int i=0; i<K; i++){
-//			centers[i] = inputData[indices.get(i)];
-//		}
-//
-//    	Arrays.fill(sizes, 0.5); //Use your intuition
+	//Initialize centers as a random subset of the patterns
+	private void initializeCenters(int[] structure) {
+		System.out.println(patterns.length);
+		centers = new double[M][][];
+		for(int i=0; i<M; i++){
+			//Contains unique indices for centers (chosen from the inputData)
+			List<Integer> indices = new ArrayList<Integer>();
+			//Generate K unique indices
+			Random rand = new Random();
+			while(indices.size()<structure[i]){
+				int randomIndex = rand.nextInt(patterns.length);
+				if(indices.contains(randomIndex)){
+					continue;
+				}
+				indices.add(randomIndex);
+			}
+			centers[i] = new double[structure[i]][N];		
+			//Use these indices to initialize centers
+			for(int j=0; j<structure[i]; j++){
+				centers[i][j] = patterns[indices.get(j)];
+			}
+		}
 	}
 	
+	//Patterns drawn randomly, equally distributed 
+	//from 3 non-overlapping areas within the unit cube
 	private void findPatterns() {
-//		TODO fill patterns and tMax = 1000 from non overlapping circles
+		patterns = new double[tMax][N];
+		//double[] randomPattern = new double[N];
+		Random rand = new Random();
+		for(int i=0; i<tMax; i++){
+			//Choose one out of 3 areas at random
+			int area = rand.nextInt(99)%3;
+			//Get a random point from this area	
+			patterns[i] = getRandomPoint(area);
+		}
+	}
+
+	//Given a number of the area return a random point from this area
+	private double[] getRandomPoint(int area){
+		//Split into 3 non-overlapping areas:
+		//[0.0, 0.3), [0.35, 0.65), [0.7, 1.0) for each coordinate
+		double[] randomPoint = new double[N];
+		switch(area){
+			case 0: randomPoint = drawRandomPoint(0.0, 0.3);
+					break;
+			case 1: randomPoint = drawRandomPoint(0.35, 0.65);
+					break;
+			case 2: randomPoint = drawRandomPoint(0.7, 1.0);
+					break;
+		}
+	return randomPoint;
+	}
+
+	private double[] drawRandomPoint(double minValue, double maxValue){
+		double[] randomPoint = new double[N];
+		Random rand = new Random();
+		for(int i=0; i<N; i++){
+			double randCoordinate = minValue + (maxValue - minValue) * rand.nextDouble();
+			randomPoint[i] = randCoordinate;
+		}
+		return randomPoint;
 	}
 	
 	private double rate(int t) {
@@ -118,27 +164,62 @@ public class MultiGas {
 	}
 	
 	private void printOutCenters(String outFilename) {
-//		TODO print centers by lines
+ 		
+	}
+
+	//Test random sampling
+	public void writePatternsToFile(){
+		try{
+			PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("patterns.dat")));
+  			for (int i = 0; i < tMax; i++) {
+    			for(int j=0; j<N; j++){
+    				fout.printf("%f ", patterns[i][j]);
+    			}
+    			fout.printf("\n");
+  			}
+  			fout.close();
+		}
+		catch (IOException e){
+			// if any I/O error occurs
+         	e.printStackTrace();
+		} 
+	}
+
+	public void printParams(){
+		System.out.println("=============================");
+		for(int i=0; i<M; i++){
+			System.out.println("Neron Gas " + String.valueOf(i));
+			for(int j=0; j<centers[i].length; j++){
+				System.out.println(Arrays.toString(centers[i][j]));
+			}
+			System.out.println("");
+ 		}
+ 		System.out.println("=============================");
 	}
 	
 	public static void main(String[] args) {
 //		number of neurons
 		int K = 10;
 //		number of partners
-		int M = 4;
-		int[] structure = {2,2,3,3};
+		int M = 5;
+		int[] structure = {2, 2, 3, 3, 7}; //the length of this array should be always equal M
 //		input dimension
-		int N = 3;
+		int N = 2;
+		String patternsFilename = "train_PA-E.dat";
+		int tMax = 1000;
+
 		
-		MultiGas gas = new MultiGas(K, M, N, structure);
-		
+		MultiGas gas = new MultiGas(K, M, N, tMax, structure);
+		gas.printParams();
+		//For testing
+		gas.writePatternsToFile();
 		Winner currentWinner;
-		for(int t=0; t<gas.tMax; t++) {
-			currentWinner = gas.applyStimulus(t);
-			gas.changeWinnerCenters(currentWinner.gasIndex, t);
-		}
+		// for(int t=0; t<gas.tMax; t++) {
+		// 	currentWinner = gas.applyStimulus(t);
+		// 	gas.changeWinnerCenters(currentWinner.gasIndex, t);
+		// }
 		
-		gas.printOutCenters("PA-E.net");
+		// gas.printOutCenters("PA-E.net");
 	}
 	
 	public class Winner {
