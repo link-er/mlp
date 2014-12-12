2720f68a6cb169303825c8f8575fb6dc64985d3d
link_er <link_er@bk.ru>
2014-12-12 11:45:48 +0100

    hopfield

--- Hopfield.java
+++ Hopfield.java
@@ -1,19 +1,22 @@
+
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 public class Hopfield {
-
 	private int[][] weights;
 	private int[] state;
 	private int[] thresholds; // Do we need doubles?
 	private int K;
 	private boolean areThresholdsSet = false;
+	public static int[][] patterns;
 
 	// Constructor with setting all thresholds
 	public Hopfield(int K, int threshold) {
 		this.K = K;
 		weights = new int[K][K];
-		Arrays.fill(weights, 0);
+		for(int i=0; i<K; i++)
+			Arrays.fill(weights[i], 0);
 		state = new int[K];
 		thresholds = new int[K];
 		setThresholds(threshold);
@@ -23,26 +26,41 @@ public class Hopfield {
 	public Hopfield(int K) {
 		this.K = K;
 		weights = new int[K][K];
-		Arrays.fill(weights, 0);
+		for(int i=0; i<K; i++)
+			Arrays.fill(weights[i], 0);
 		state = new int[K];
 		thresholds = new int[K];
 	}
 
-	public void recall(int[] pattern) {
-		System.arraycopy(pattern, 0, state, 0, K);
-		int[] previousState = new int[K];
-		boolean converged = false;
-		int neuronIndex;
-		Random rand = new Random();
-		while (!converged) {
-			// 0. Save the state before changing
-			System.arraycopy(state, 0, previousState, 0, K);
+//	for random taking neuron
+	private int[] shuffleNeuronIndecis() {
+		int[] result = new int[K];
+		for(int i=0; i<K; i++)
+			result[i] = i;
+		Collections.shuffle(Arrays.asList(result));
+		return result;
+	}
 
+	public void recall(int[] pattern) {
+		state = pattern;
+		int[] previousState = new int[K];
+//		on first step previous state is not equal to state
+		Arrays.fill(previousState, -2);
+		boolean converged = false;
+		int neuronIndex = 0;
+		int sum=0;
+		int[] sequenseOfFiring = shuffleNeuronIndecis();
+		while (!converged) {
 			// 1. Choose the index at random
-			neuronIndex = rand.nextInt(K);
+			neuronIndex = sequenseOfFiring[neuronIndex];
+
 			// 2. Calculate weighted sum for this neuron
+			sum = 0;
+			for(int i=0; i<K; i++)
+				sum += weights[i][neuronIndex]*pattern[i];
 
 			// 3. Compare to threshold: change the state
+			state[neuronIndex] = (sum > thresholds[neuronIndex]) ? 1 : -1;
 
 			// 4. Print the state
 			printState();
@@ -51,6 +69,14 @@ public class Hopfield {
 			if (Arrays.equals(state, previousState)) {
 				converged = true;
 			}
+			System.arraycopy(state, 0, previousState, 0, K);
+
+			if(neuronIndex==K-1) {
+				sequenseOfFiring = shuffleNeuronIndecis();
+				neuronIndex = 0;
+			}
+			else
+				neuronIndex ++;
 		}
 
 		System.out.println("Result (new converged state): "
@@ -59,10 +85,17 @@ public class Hopfield {
 	}
 
 	public void learnPattern(int[] pattern) {
+//		set thresholds to the first pattern
 		if (!areThresholdsSet) {
 			thresholds = pattern;
 		}
-		// Do something with weight matrix
+
+//		learn the pattern
+		for(int i=0; i<K; i++)
+			for(int j=0; j<K; j++) {
+				if(i!=j)
+					weights[i][j] += pattern[i]*pattern[j];
+			}
 	}
 
 	private void setThresholds(int thresholdValue) {
@@ -72,11 +105,12 @@ public class Hopfield {
 
 	private void printState() {
 		if (K < 101) {
+//			# stands for 1, _ stands for 0
 			for (int i = 0; i < state.length; i++) {
 				if (state[i] == 1) {
 					System.out.print("#");
 				} else {
-					System.out.print(".");
+					System.out.print("_");
 				}
 			}
 		} else {
@@ -86,21 +120,61 @@ public class Hopfield {
 	}
 
 	private double getEnergy() {
-
+		int sum1 = 0, sum2 = 0;
+		for(int i=0; i<K; i++) {
+			sum2 += thresholds[i]*state[i];
+			for(int j=0; j<K; j++)
+				sum1 += weights[i][j]*state[i]*state[j];
+		}
+		return -0.5*sum1 + sum2;
 	}
 
-	private public static void main(String[] args) {
-
+	public static void main(String[] args) {
 		/******************************
 		 * Set parameters here
 		 ******************************/
 		// Number of neurons
-		int K;
+		int K = 150;
+//		value for threshold
+		int thresholdValue = 0;
+//		number of patterns
+		int patternsCount = 10;
+//		patterns
+		Random rand = new Random();
+		patterns = new int[patternsCount][K];
+		for(int i=0; i<patternsCount; i++)
+			for(int j=0; j<K; j++) {
+				if(rand.nextBoolean())
+					patterns[i][j] = 1;
+				else
+					patterns[i][j] = -1;
+			}
+
+		System.out.println("Patterns:");
+		for(int[] pattern : patterns)
+			System.out.println(Arrays.toString(pattern));
+		System.out.println();
 
 		/******************************
 		 * Set parameters here
 		 ******************************/
+//		uncomment to get user defined values for threshold or 0
+		Hopfield net = new Hopfield(K, thresholdValue);
+//		uncomment to get threshold from the starting pattern
+//		Hopfield net = new Hopfield(K);
 
+//		learn to patterns
+		for(int[] pattern : patterns)
+			net.learnPattern(pattern);
+
+//		recalling
+//		change 3d part of the first pattern to check convergency
+		int[] input = new int[K];
+		System.arraycopy(patterns[0], 0, input, 0, K);
+		for(int i=0; i<K/3; i++)
+			input[i] *= -1;
+		net.recall(input);
+
+		System.out.println(Arrays.equals(patterns[0], net.state));
 	}
-
 }
